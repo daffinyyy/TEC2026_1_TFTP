98ec73fa9d2d474ab0c0a3b7fed3b133ca994dc6
aglisonnn <aglisonmn@gmail.com>
2026-03-15 16:13:02 -0400

    feat: server and client CLI interface

---
+++ cli/client_cli.py
@@ -0,0 +1,23 @@
+import argparse
+
+from tftp.client import TFTPClient
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("server")
+    parser.add_argument("command", choices=["get"])
+    parser.add_argument("filename")
+
+    args = parser.parse_args()
+
+    client = TFTPClient(args.server)
+
+    if args.command == "get":
+        client.download(args.filename)
+
+
+if __name__ == "__main__":
+    main()
